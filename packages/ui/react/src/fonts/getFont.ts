4efdf3c04fac6f71d110a0095ccdd26490a7a88b
Thales Menezes <thlmenezes.dev@gmail.com>
2024-10-09 09:57:56 -0300

    feat: comment font mono source

--- packages/ui/react/src/fonts/getFont.ts
+++ packages/ui/react/src/fonts/getFont.ts
@@ -3,6 +3,15 @@ import get from 'lodash/get'
 const fonts = {
   roboto: 'Roboto',
   montserrat: 'Montserrat',
+  /**
+   * Font Family Mono
+   *
+   * Code obtained from TailwindCSS
+   * Source code from tailwindlabs @ https://github.com/tailwindlabs/tailwindcss
+   *
+   * @see {@link https://tailwindcss.com/docs/font-family | TailwindCSS}
+   * @see {@link https://github.com/tailwindlabs/tailwindcss/blob/b1733ac1fd19cd84a4cf973cdc1630a1a879e356/packages/tailwindcss/theme.css#L371-L372}
+   */
   mono: 'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
 } as const
 
